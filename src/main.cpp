1253559dcf1cd02e9eb72a9757613c889cab059a
vb8146649 <vb8146649@gmail.com>
2025-12-05 23:05:40 +0530

    doesnt work with namespace for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  cout << "$ ";
+  std::cout << "$ ";
 }
