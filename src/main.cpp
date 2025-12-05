c724446f79af00c169f0c1529f867ec6aa431161
vb8146649 <vb8146649@gmail.com>
2025-12-05 23:04:24 +0530

    used namespace std

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  // std::cout << "$ ";
+  cout << "$ ";
 }
